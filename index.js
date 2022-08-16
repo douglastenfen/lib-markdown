a8c7c88b12d6f9ecc70b07e9e0c0f9cc0ece0fc3
douglastenfen <douglastenfen@gmail.com>
2022-08-16 19:29:49 -0300

    error handling function

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const chalk = require("chalk");
 
 const fs = require("fs");
 
+function handleError(error) {
+  throw new Error(chalk.red(error.code, "there isn't archive in the path"));
+}
+
 function getArchive(archivePath) {
   const encoding = "utf-8";
-  fs.readFile(archivePath, encoding, (_, data) => {
+  fs.readFile(archivePath, encoding, (error, data) => {
+    if (error) {
+      handleError(error);
+    }
+
     console.log(chalk.green(data));
   });
 }
